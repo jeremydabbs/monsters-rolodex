9bb170a05cf3030c1c01f415ec2fce37ed85c403
Jeremy <jndabbs@gmail.com>
2020-10-22 15:17:22 -0500

    Updates

---
+++ precache-manifest.4a4224653582745c5ca8d9a8d6c1c13b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53472d2c0af9a31adf241161b36fe830",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "ecb5fa408a14fea8548b",
+    "url": "/monsters-rolodex/static/css/main.11c2cad1.chunk.css"
+  },
+  {
+    "revision": "f446d9444977f330bfbe",
+    "url": "/monsters-rolodex/static/js/2.70969f69.chunk.js"
+  },
+  {
+    "revision": "65075b20798fa24012a1ad453c78c90d",
+    "url": "/monsters-rolodex/static/js/2.70969f69.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ecb5fa408a14fea8548b",
+    "url": "/monsters-rolodex/static/js/main.7126c1a0.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
